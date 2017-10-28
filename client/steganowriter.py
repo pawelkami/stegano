e9755c233faa8d967e4967350f15624935cf99da
pawelkami <pawelkami2@gmail.com>
2017-10-28 14:29:17 +0200

    added antygona.txt

--- client/steganowriter.py
+++ client/steganowriter.py
@@ -1,6 +1,13 @@
+#!/bin/python
+#-*- coding: utf-8 -*-
+
 from netfilterqueue import NetfilterQueue
 import socket
 
+def readFile(filename):
+    with open(filename, 'rb') as f:
+        data = f.read()
+        return data		
 
 def print_and_accept(pkt):
     print(pkt)
@@ -9,6 +16,9 @@ def print_and_accept(pkt):
 nfqueue = NetfilterQueue()
 nfqueue.bind(0, print_and_accept)
 s = socket.fromfd(nfqueue.get_fd(), socket.AF_UNIX, socket.SOCK_STREAM)
+
+textToHide = readFile("/stegano/Antygona.txt")
+
 try:
     nfqueue.run_socket(s)
 except KeyboardInterrupt:
