a681db5c70d9b3b29e4720fd9f4b4ed0bd56ecef
pawelkami <pawelkami2@gmail.com>
2017-11-01 21:19:47 +0100

    Added ecnryption

--- client/steganowriter.py
+++ client/steganowriter.py
@@ -5,6 +5,42 @@ from netfilterqueue import NetfilterQueue
 from scapy.all import *
 import socket
 
+def xor(data, key):
+    key = bytearray(key)
+    data = bytearray(data)
+
+    out = bytes()
+    for i, d in enumerate(data):
+        out += bytes([d ^ key[i % len(key)]])
+
+    return out
+
+
+def encrypt(message, key):
+    return xor(message, key)
+
+def getEncryptKey(scapy_packet):
+    pkt = scapy_packet.getlayer(IP)
+    return bytes([pkt.id & 0xff])
+
+def prepareMessage(scapy_packet):
+    global textToHide
+    global byte_num
+    tcp_pkt = scapy_packet.getlayer(TCP)
+
+    message = bytes()
+    for i in range(2):
+        message += bytes([textToHide[byte_num]])
+        byte_num += 1
+
+    print("Plain message: " + message.decode("windows-1250"))
+    message = encrypt(message, getEncryptKey(scapy_packet))
+
+    tcp_pkt.flags &= 0xef
+    tcp_pkt.urgptr = (message[0] << 8) | (message[1])
+
+    return scapy_packet
+
 
 def read_file(filename):
     with open(filename, 'rb') as f:
@@ -24,11 +60,8 @@ def modify(packet):
     global byte_num
 
     if byte_num < len(textToHide):
-        tcp_pkt.flags &= 0xef       # mask which disabled urgent pointer
-        tcp_pkt.urgptr = (textToHide[byte_num] << 8) | (textToHide[byte_num + 1])
-        byte_num += 2
+        scapy_pkt = prepareMessage(scapy_pkt)
 
-    print('Secret message: ' + chr(tcp_pkt.urgptr))
     sendp(scapy_pkt)
     packet.drop()
 
--- server/steganoreader.py
+++ server/steganoreader.py
@@ -3,6 +3,34 @@ from scapy.all import *
 
 stars = lambda n: "*" * n
 
+def xor(data, key):
+    key = bytearray(key)
+    data = bytearray(data)
+
+    out = bytes()
+    for i, d in enumerate(data):
+        out += bytes([d ^ key[i % len(key)]])
+
+    return out
+
+
+def decrypt(message, key):
+    return xor(message, key)
+
+def getEncryptKey(scapy_packet):
+    pkt = scapy_packet.getlayer(IP)
+    return bytes([pkt.id & 0xff])
+
+def getHiddenMessage(scapy_packet):
+    tcp_pkt = scapy_packet.getlayer(TCP)
+    message = bytes()
+
+    message += bytes([tcp_pkt.urgptr >> 8])
+    message += bytes([tcp_pkt.urgptr & 0xff])
+    message = decrypt(message, getEncryptKey(scapy_packet))
+
+    return message
+
 secret_messages = {}
 
 def read(packet):
@@ -12,7 +40,7 @@ def read(packet):
 
     tcp_packet = packet.getlayer(TCP)
 
-    secret_messages[packet[IP].src] += bytes([tcp_packet.urgptr >> 8, tcp_packet.urgptr & 0xff])
+    secret_messages[packet[IP].src] += getHiddenMessage(packet) #bytes([tcp_packet.urgptr >> 8, tcp_packet.urgptr & 0xff])
     print(secret_messages[packet[IP].src])
 
 def GET_print(packet):
