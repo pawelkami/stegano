07507885968119252c37d348f88bbad4126e49b5
pawelkami <pawelkami2@gmail.com>
2017-11-02 01:29:15 +0100

    Ultra fast messaging

--- client/client.py
+++ client/client.py
@@ -1,13 +1,8 @@
 import time
-import random
-import os
 import pycurl
 
 while True:
-    time.sleep(0.2)
-
-    # -p: download website with CSS, images, etc.
-    # -H: download resources from external hosts too
-    # -e robots=off: ignore robots.txt (sometimes robots.txt doesn't allow downloading full website)
-    # -nv: less verbose output
-    os.system('wget -P downloads -p -H -e robots=off -nv server')
+    time.sleep(0.1)
+    c = pycurl.Curl()
+    c.setopt(c.URL, "server")
+    c.perform()
