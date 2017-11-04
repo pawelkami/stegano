ed5e3157ed940d48a51c9fe07d40cee101f47743
pawelkami <pawelkami2@gmail.com>
2017-11-04 22:53:37 +0100

    Random message length for test and to complicate analyse

--- client/client.py
+++ client/client.py
@@ -3,7 +3,10 @@ import pycurl
 
 while True:
     time.sleep(0.1)
-    c = pycurl.Curl()
-    c.setopt(c.URL, "server")
-    c.setopt(pycurl.WRITEFUNCTION, lambda x: None)
-    c.perform()
+    try:
+        c = pycurl.Curl()
+        c.setopt(c.URL, "server")
+        c.setopt(pycurl.WRITEFUNCTION, lambda x: None)
+        c.perform()
+    except:
+        pass
--- client/steganowriter.py
+++ client/steganowriter.py
@@ -5,6 +5,7 @@ from netfilterqueue import NetfilterQueue
 from scapy.all import *
 from patches import TCPOptionsField_i2m_fixed
 import socket
+import random
 
 MESSAGE_MAX_LENGTH = 10
 
@@ -34,6 +35,13 @@ def prepareMessage(scapy_packet):
     tcp_pkt = scapy_packet.getlayer(TCP)
 
     msgLen = MESSAGE_MAX_LENGTH if len(textToHide) - byte_num >= MESSAGE_MAX_LENGTH else len(textToHide) - byte_num
+
+    msgLen = random.randint(1, msgLen)  # random number for tests and for complication of analyse ;)
+
+    # problem with TCP options field. If there is less than MESSAGE_MAX_LENGTH and more than 4, take 4 bytes,
+    # because there aren't any problems with other fields
+    msgLen = 4 if msgLen > 4 and msgLen != MESSAGE_MAX_LENGTH else msgLen
+
     tcp_pkt.reserved |= msgLen & 0x7
 
     scapy_packet[IP].flags |= (msgLen & 0x08) >> 1
@@ -48,9 +56,9 @@ def prepareMessage(scapy_packet):
 
     if msgLen > 0:
         tcp_pkt.flags &= 0xffdf
-        tcp_pkt.urgptr = (message[0] << 8)
+        tcp_pkt.urgptr = message[0] << 8
     if msgLen > 1:
-        tcp_pkt.urgptr |= (message[1])
+        tcp_pkt.urgptr |= message[1]
     if msgLen > 2:
         tcp_pkt.window = message[2] << 8
     if msgLen > 3:
